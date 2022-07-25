ce0c5a7a3aff659eb5107b5a7e0009bd5df8dcd5
Utkarsh Gupta <63789702+utkarshgupta04092003@users.noreply.github.com>
2022-07-25 12:00:23 +0530

    Create 01 Reverse a String.java

---
+++ Arrays/01 Reverse a String.java
@@ -0,0 +1,45 @@
+/*************************************************************
+
+You are given a string s. You need to reverse the string.
+
+Example 1:
+
+Input:
+s = Geeks
+Output: skeeG
+Example 2:
+
+Input:
+s = for
+Output: rof
+Your Task:
+
+You only need to complete the function reverseWord() that
+takes s as parameter and returns the reversed string.
+
+Expected Time Complexity: O(|S|).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= |s| <= 10000
+
+************************************************************/
+
+
+class Reverse
+{
+    // Complete the function
+    // str: input string
+    public static String reverseWord(String str)
+    {
+        // Reverse the string str
+        String rev = "";
+        int n = str.length();
+        for(int i=n-1;i>=0;i--){
+            rev += str.charAt(i);
+        }
+        return rev;
+    }
+}
+
+// Problem Link - https://practice.geeksforgeeks.org/problems/reverse-a-string/1
