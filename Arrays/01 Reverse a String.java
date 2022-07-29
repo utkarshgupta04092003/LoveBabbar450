ae1e4375015263dae423b2794ae22cf864b8a98c
Utkarsh Gupta <63789702+utkarshgupta04092003@users.noreply.github.com>
2022-07-29 14:45:12 +0530

    commit;

--- Arrays/01 Reverse a String.java
+++ Arrays/01 Reverse a String.java
@@ -23,10 +23,6 @@ Expected Auxiliary Space: O(1).
 Constraints:
 1 <= |s| <= 10000
 
-
-
-we can also solve how to reverse an array in O(n) time
-
 ************************************************************/
 
 
